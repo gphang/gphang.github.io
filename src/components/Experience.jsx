9e6fc8aa452b4e3fad812d12cdcce9f687a0e56f
gphang <gphang@umich.edu>
2025-08-20 02:54:03 +0100

    fix: minor title font inconsistencies

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Experience = () => {
     return (
-        <div id='experience' className='flex min-h-screen w-full flex-col items-center justify-center gap-20  p-14 md:p-20 lg:p-36'>
+        <div id='experience' className='min-h-screen w-full flex flex-col gap-20 p-10 md:p-16 xl:p-32'>
             <h1 className='text-center text-5xl font-light'>Experience</h1>
 
             <div className="flex flex-wrap gap-0 md:flex-nowrap md:gap-10">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id='projects' className='min-h-screen w-full flex flex-col gap-20 p-10 md:p-16 xl:p-32 '>
+    <div id='projects' className='min-h-screen w-full flex flex-col gap-20 p-10 md:p-16 xl:p-32'>
       <h1 className='text-center text-5xl font-light'>Projects</h1>
 
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-10'>
